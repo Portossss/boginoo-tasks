645c26f56fa9238f200487106a57ebc61244a207
Yalguun Erkhembayar <yalguunerkhembayar@Portoss-IMAC.local>
2020-06-29 18:45:07 +0800

    test

--- src/pages/home-default.js
+++ src/pages/home-default.js
@@ -10,12 +10,12 @@ export const HomeDefault = () => {
                     <IconDash />
                     <IconEndBracket />
                 </div>
-                <div className='font-lobster c-primary fs-56 lh-70'>
+                <div className='font-lobster flex justify-center c-primary fs-56 lh-70 mb-6'>
                     Boginoo
                 </div>
                 <div className='mt-5 flex justify-center items-center'>
-                    <Input className="h-5 w-8 c-primary" placeholder='https://www.web-huudas.mn' />
-                    <Button className='font-ubuntu fs-20 lh-23 bold c-default h-5 ph-4 ml-4 b-primary' disabled={true}>Богиносгох</Button>
+                    <Input className="h-5 w-9 c-primary" placeholder='https://www.web-huudas.mn' />
+                    <Button className='font-ubuntu fs-20 lh-23 bold c-default h-5 ph-4 ml-4 b-primary'>Богиносгох</Button>
                 </div>
             </div>
         </Layout>
